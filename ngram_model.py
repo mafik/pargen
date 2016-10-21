8168bd9c5142b524fad40f0ab76c53670bc0af99
Marek Rogalski <mafikpl@gmail.com>
2016-10-21 11:14:30 +0200

    Moved training full on-GPU

--- ngram_model.py
+++ ngram_model.py
@@ -34,7 +34,7 @@ checkpoint_name = makedir("checkpoints/" + args.RUN_NAME) + "/model.ckpt"
 restore = args.restore
 ngram_len = args.ngram_length
 
-train_time = float("inf") # 600
+train_time = 0 # 300
 checkpoint_time = 10 if args.quick else 600
 test = True
 gen = False
@@ -45,16 +45,12 @@ log_start()
 with status("Reading training data... "):
   train_set, valid_set, test_set, alphabetic_symbol_map, symbol_count = data.read_nkjp(ngram_length=ngram_len)
 
-# Training parameters
-
-
 saver = None
 
 # TODO: gen mode with bigger batch size
-# TODO: replace rare symbols with UNKs!
-# TODO: cache read_nkjp
-# TODO: properly initialize all the variables
+# TODO: carefully initialize all the variables
 
+gather_check_indicies = False
 class Model:
   reuse_variables = None
   variables_loaded = False
@@ -63,19 +59,57 @@ class Model:
       self.mode = mode
       self.embedding_size = 10
       self.network_size = 200
-      self.batch_size = 1 if self.mode == 'gen' else 50
+      self.batch_size = 1 if self.mode == 'gen' else 500
       self.max_sequence_length = 1 if mode == 'gen' else (2 if args.quick else 100)
       dropout_keep_prob = 0.5 if mode == 'train' else 1 # 0.9
       self.num_layers = 2
       init_scale = 0.05
       initializer = tf.random_uniform_initializer(-init_scale, init_scale)
 
-      self.sequence_lengths = tf.placeholder(tf.int32, [self.batch_size], "sequence_lengths")
-      #self.input_state = [tuple([tf.placeholder(tf.float32, [self.batch_size, self.network_size], "input_state") for part in "cm"]) for i in range(self.num_layers)]
-      self.input_state = tuple([rnn_cell.LSTMStateTuple(*[tf.placeholder(tf.float32, [self.batch_size, self.network_size], "input_state") for part in "cm"]) for i in range(self.num_layers)])
 
-      self.sequence = tf.placeholder(tf.int32, [self.batch_size, self.max_sequence_length, ngram_len], "sequence")
-      self.chars = tf.reduce_sum(self.sequence_lengths)
+      # in test and train modes - upload full sequence as a constant and accept input indicies
+      # in gen mode - accept input sequence
+      if mode == 'train':
+        sample_offsets = [0] # the last one is the end of sequence
+        for l in train_set.lengths:
+          sample_offsets.append(sample_offsets[-1] + l)
+        self.flat_dataset = tf.constant(np.concatenate(train_set.arrays), name="flat_dataset")
+        self.dataset_sample_lengths = tf.constant(train_set.lengths, name="dataset_sample_lengths")
+        self.dataset_sample_offsets = tf.constant(sample_offsets, name="dataset_sample_offsets")
+        initial_random_sample = tf.random_uniform([self.batch_size], minval=0, maxval=train_set.size, dtype=tf.int32, name="initial_random_sample")
+        self.sample = tf.Variable(initial_random_sample, name="sample")
+        self.zero_offsets = tf.zeros([self.batch_size], tf.int32, name="zero_offsets")
+        self.sample_offsets = tf.Variable(self.zero_offsets, name="sample_offsets")
+
+        self.sample_lengths = tf.gather(self.dataset_sample_lengths, self.sample, gather_check_indicies, name="sample_lengths")
+        self.sample_indicies = tf.gather(self.dataset_sample_offsets, self.sample, gather_check_indicies, name="gather_dataset_sample_indicies")
+        self.sample_indicies = tf.add(self.sample_indicies, self.sample_offsets, name="shift_sample_indicies")
+        self.remaining_sample_lengths = tf.sub(self.sample_lengths, self.sample_offsets)
+        self.gather_indicies = tf.add(
+          tf.tile(tf.expand_dims(self.sample_indicies, 1), [1, self.max_sequence_length]),
+          tf.minimum(
+            tf.tile(tf.expand_dims(tf.range(self.max_sequence_length), 0), [self.batch_size, 1]),
+            tf.tile(tf.expand_dims(tf.sub(self.remaining_sample_lengths, tf.constant(1)), 1), [1, self.max_sequence_length]),
+            name="limit_indicies"),
+          name="indicies_to_gather")
+        self.sequence = tf.gather(self.flat_dataset, self.gather_indicies, gather_check_indicies, name="sequence")
+        self.consumed_sequence_lengths = tf.minimum(self.remaining_sample_lengths, tf.constant(self.max_sequence_length))
+        self.sequence_lengths = self.consumed_sequence_lengths
+        self.initial_state = tf.zeros([self.batch_size, self.network_size], name="initial_zero_state")
+        self.input_state = tuple([rnn_cell.LSTMStateTuple(
+          *[tf.Variable(self.initial_state, trainable=False, name="input_state_{}_{}".format(part, i)) for part in "ch"]) for i in
+                                  range(self.num_layers)])
+        # TODO: remove check indicies from gather
+
+
+      else:
+        self.sequence_lengths = tf.placeholder(tf.int32, [self.batch_size], "sequence_lengths")
+        self.sequence = tf.placeholder(tf.int32, [self.batch_size, self.max_sequence_length, ngram_len], "sequence")
+        self.input_state = tuple([rnn_cell.LSTMStateTuple(
+          *[tf.placeholder(tf.float32, [self.batch_size, self.network_size], "input_state") for part in "ch"]) for i in
+                                  range(self.num_layers)])
+
+      self.chars = tf.reduce_sum(self.sequence_lengths, name="chars")
 
       summary_list = []
 
@@ -88,76 +122,77 @@ class Model:
           cell = rnn_cell.MultiRNNCell([cell] * self.num_layers, state_is_tuple=True)
         if self.network_size != self.embedding_size * ngram_len:
           cell = rnn_cell.InputProjectionWrapper(cell, num_proj=self.network_size)
-        if len(alphabetic_symbol_map) != self.network_size:
-          cell = rnn_cell.OutputProjectionWrapper(cell, output_size=len(alphabetic_symbol_map))
-        self.cell = cell
         # this is list of tensors of shape=(batch_size, ngram_len)
-        # TODO: sequence_list = tf.unpack(self.sequence, axis=1)
-        sequence_list = [tf.squeeze(t, [1]) for t in tf.split(1, self.max_sequence_length, self.sequence)]
-        embeddings = [tf.nn.embedding_lookup(embedding_matrix, item) for item in (sequence_list[:-1] if self.mode != 'gen' else sequence_list)]
+        sequence_list = tf.unpack(self.sequence, axis=1)
+        #sequence_list = [tf.squeeze(t, [1]) for t in tf.split(1, self.max_sequence_length, self.sequence)]
+        embeddings = [tf.nn.embedding_lookup(embedding_matrix, item) for item in sequence_list]
         embeddings = [tf.reshape(time_step, shape=(self.batch_size, self.embedding_size * ngram_len)) for time_step in embeddings]
+        cell = rnn_cell.OutputProjectionWrapper(cell, output_size=symbol_count)
         self.logits, self.state = rnn.rnn(cell,
-                                           embeddings,
+                                           embeddings[:-1],
                                            initial_state=self.input_state,
                                            sequence_length=self.sequence_lengths)
+
         #argmax = [tf.argmax(logit, 1) for logit in logits]
         if mode != 'gen':
-          losses = []
-          for i in range(self.max_sequence_length-1):
-            label = tf.squeeze(tf.split(1, ngram_len, sequence_list[i + 1])[0], [1])
-            losses.append(tf.nn.sparse_softmax_cross_entropy_with_logits(self.logits[i], label))
-          offset_costs = [tf.reduce_sum(loss) for loss in losses]
-          for i, cost in enumerate(offset_costs):
-            summary_list.append(tf.scalar_summary(mode + '_offset_'+str(i)+'_cost', cost))
+          with tf.name_scope("offset_costs", [self.max_sequence_length] + self.logits + sequence_list):
+            losses = []
+            for i in range(self.max_sequence_length-1):
+              next_labels = tf.split(1, ngram_len, sequence_list[i + 1])
+              for j in range(1): # ngram_len):
+                label = tf.squeeze(next_labels[j], [1])
+                losses.append(tf.nn.sparse_softmax_cross_entropy_with_logits(self.logits[i], label))
+
+            offset_costs = [tf.reduce_sum(loss) for loss in losses]
+          #for i, cost in enumerate(offset_costs):
+          #  summary_list.append(tf.scalar_summary(mode + '_offset_'+str(i)+'_cost', cost))
           self.total_cost = tf.add_n(offset_costs)
-          summary_list.append(tf.scalar_summary(mode + '_total_cost', self.total_cost))
-          summary_list.append(tf.scalar_summary(mode + '_chars', self.chars))
-          summary_list.append(tf.histogram_summary(mode + '_sequence_lengths', self.sequence_lengths))
+          #summary_list.append(tf.scalar_summary(mode + '_total_cost', self.total_cost))
+          #summary_list.append(tf.scalar_summary(mode + '_chars', self.chars))
+          #summary_list.append(tf.histogram_summary(mode + '_sequence_lengths', self.sequence_lengths))
           self.cost_per_char = tf.div(self.total_cost, tf.cast(self.chars, tf.float32), "cost_per_char")
           summary_list.append(tf.scalar_summary(mode + '_cost_per_char', self.cost_per_char))
           if mode == 'train':
-            self.lr = tf.Variable(0.0, trainable=False)
+            self.lr = tf.Variable(1.0, trainable=False, name="learning_rate")
             summary_list.append(tf.scalar_summary('learning_rate', self.lr))
+            #summary_list.append(tf.histogram_summary('sampled_examples', self.random_sample))
             optimizer = tf.train.AdagradOptimizer(self.lr)
             tvars = tf.trainable_variables()
             grad = tf.clip_by_global_norm(tf.gradients(self.total_cost, tvars), 5)[0]
-            self.train_op = optimizer.apply_gradients(zip(grad, tvars))
-      self.summaries = tf.merge_summary(summary_list)
+            apply_gradients = optimizer.apply_gradients(zip(grad, tvars))
+            final_sample_offsets = tf.add(self.sample_offsets, self.consumed_sequence_lengths)
+            finished_samples_mask = tf.greater_equal(final_sample_offsets, self.sample_lengths)
+            random_sample = tf.random_uniform([self.batch_size], minval=0, maxval=train_set.size, dtype=tf.int32, name="random_sample")
+            clean_finished_samples = tf.assign(self.sample, tf.select(finished_samples_mask, random_sample, self.sample), name="clean_finished_samples")
+            next_sample_offsets = final_sample_offsets # tf.sub(final_sample_offsets, tf.constant(1))
+            advance_sample_offsets = tf.assign(self.sample_offsets, tf.select(finished_samples_mask, self.zero_offsets, next_sample_offsets), name="advance_sample_offsets")
+            copy_network_state_ops = []
+            for layer_state_var, new_layer_state in zip(self.input_state, self.state):
+              for state_part_var, new_state_part in zip(layer_state_var, new_layer_state):
+                copy_network_state_ops.append(tf.assign(state_part_var, tf.select(finished_samples_mask, self.initial_state, state_part_var)))
+            copy_network_state = tf.group(*copy_network_state_ops, name="copy_network_state")
+            self.train_op = tf.group(apply_gradients, clean_finished_samples, advance_sample_offsets, copy_network_state, name="train_op")
+          self.summaries = tf.merge_summary(summary_list) # move this out if gen will have some summaries
 
       Model.reuse_variables = True # future invocations will reuse the same variables
 
   def zero_state(self):
-    #return self.cell.zero_state(self.batch_size, np.float32)
     return [tuple([np.zeros([self.batch_size, self.network_size], dtype=np.float32) for part in "cm"]) for i in range(self.num_layers)]
 
   def train(self):
     global checkpoint_entropy
     i = 1
-    initial_lr = 0.8
-    falloff = 1
-    train_writer = tf.train.SummaryWriter(summary_dir, session.graph)
-
+    with status("Initializing SummaryWriter"):
+      train_writer = tf.train.SummaryWriter(summary_dir, session.graph)
     last_checkpoint = clock()
     training_started = clock()
     while clock() - training_started < train_time:
       t = clock() - training_started
-      session.run(tf.assign(self.lr, initial_lr * (falloff ** (t / 10))))
-      indicies = np.random.randint(len(train_set.lines), size=self.batch_size)
-      args = {self.sequence_lengths: np.minimum(self.max_sequence_length, train_set.lengths[indicies])}
-
-      for layer_placeholder, layer_state in zip(self.input_state, self.zero_state()):
-        for part_placeholder, part_state in zip(layer_placeholder, layer_state):
-          args[part_placeholder] = part_state
-
-      args[self.sequence] = np.zeros((self.batch_size, self.max_sequence_length, ngram_len), dtype=np.int32)
-      train_batch = train_set.arrays[indicies]
-      for sample_num in range(self.batch_size):
-        x = min(self.max_sequence_length, len(train_batch[sample_num]))
-        args[self.sequence][sample_num, :x] = train_batch[sample_num][:x]
-      summaries, cost_per_char, train_op = session.run([self.summaries, self.cost_per_char, self.train_op], args)
+      #session.run(tf.assign(self.lr, 1 * (.8 ** (t / 10))))
+      summaries, train_op = session.run([self.summaries, self.train_op], {})
       train_writer.add_summary(summaries, i)
       train_writer.flush()
-      log("Training iteration {}, training time {:.0f}s, cost {:.3f}", i, t, cost_per_char)
+      log("Training iteration {}, training time {:.0f}s", i, t)
       i += 1
       if clock() - last_checkpoint > checkpoint_time:
         validation_entropy = test_model.test(valid_set, False)
@@ -169,7 +204,6 @@ class Model:
           saver.save(session, checkpoint_name)
         else:
           print(": keeping old model with per-character entropy = {:.3f}".format(checkpoint_entropy))
-
         last_checkpoint = clock()
 
   def test(self, dataset = test_set, verbose = True):
@@ -250,8 +284,9 @@ class Model:
         "sentence": "",
         "entropy": 0,
         "state": self.zero_state(),
-        "last_symbol": 1
+        "last_symbol": 1 # alphabetic_symbol_map.index("<GO>")
       })
+      EOF = 0 # alphabetic_symbol_map.index("<EOF>")
       beam_width = 100
       branching = 10
       iterations = 400 * beam_width
@@ -271,7 +306,7 @@ class Model:
 
         results.append({
           "sentence": best["sentence"],
-          "entropy": (best["entropy"] - log(generated_distribution[0], 2)) / (len(best["sentence"]) + 1) # pow
+          "entropy": (best["entropy"] - log(generated_distribution[EOF], 2)) / (len(best["sentence"]) + 1) # pow
         })
         results.sort(key=lambda x: -x["entropy"])
         results = results[-10:]
@@ -314,14 +349,15 @@ with g.as_default():
   if gen:
     gen_model = Model('gen')
 
-  with tf.Session() as session:
+  with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
     test_result_list = []
 
     for loop in range(1):
       # print("Network {}...".format(loop + 1))
       saver = tf.train.Saver()
       with session.as_default():
-        tf.initialize_all_variables().run(session=session)
+        with status("Initializing variables"):
+          tf.initialize_all_variables().run(session=session)
         if restore:
           saver.restore(session, checkpoint_name)
           checkpoint_entropy = test_model.test(valid_set, False)
