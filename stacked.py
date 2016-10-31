f04ffed75d24462a0c41097619bee42a389794a4
Marek Rogalski <mafikpl@gmail.com>
2016-10-31 07:14:39 +0100

    Building kneser-ney lm on the training set only

--- stacked.py
+++ stacked.py
@@ -30,23 +30,28 @@ if args.run_name == 'date':
 with status("Reading NKJP..."):
   arrays, lengths, symbol_map, symbol_count = data.read_nkjp_simple()
 
+dataset_np = np.concatenate([np.concatenate(([data.GO], arr, [data.EOS])) for arr in arrays])
+train_test_divider = int(len(lengths) * 0.8)
+sequence_count = len(lengths)
+test_count = sequence_count - train_test_divider
+
 with status("Loading n-gram model..."):
   n = 5
   ngram_db_path = "ngram_db_{}.npz".format(n)
   try:
     npzfile = np.load(ngram_db_path)
-    ngram_index = npzfile["concatenated"]
-    ngram_db = npzfile["db"]
+    ngram_dataset = npzfile["ngram_dataset"]
+    ngram_probability_table = npzfile["ngram_probability_table"]
   except IOError as e:
     log("Building n-gram model from scratch...")
 
     ngrams = tuple(Counter() for i in range(n+1))
-    ngrams[n].update(ngram for array in arrays for ngram in ngram_generator(chain((data.GO,)*n, array, (data.EOS,)), n))
+    ngrams[n].update(ngram for array in arrays[:train_test_divider] for ngram in ngram_generator(chain((data.GO,)*n, array, (data.EOS,)), n))
     for i in range(n - 1, 0, -1):
       for ngram, count in ngrams[i+1].items():
         ngrams[i][ngram[1:]] += count
-    ngram_counts = tuple(sum(order.values()) for order in ngrams)
 
+    log("Precomputing unique prefixes/suffixes")
     # unique_prefixes[i][ngram] where len(ngram) == i contains number of different symbols that proceed given ngram
     unique_prefixes = tuple(Counter() for i in range(n))
     # unique_suffixes[i][ngram] where len(ngram) == i contains number of different symbols that follow given ngram
@@ -55,49 +60,72 @@ with status("Loading n-gram model..."):
       unique_prefixes[i-1].update(ngram[1:] for ngram in ngrams[i].keys())
       unique_suffixes[i-1].update(ngram[:-1] for ngram in ngrams[i].keys())
 
+    log("Indexing ngrams")
+    all_ngrams = tuple(set() for i in range(n+1))
+    for array in arrays:
+      for ngram in ngram_generator(chain((data.GO,)*n, array, (data.EOS,)), n):
+        all_ngrams[n].add(ngram)
+    for i in range(n - 1, 0, -1):
+      for ngram in all_ngrams[i+1]:
+        all_ngrams[i].add(ngram[1:])
+    # maps ngram tuple to ngram number
+    ngram_idx = tuple(dict() for i in range(n))
+    for i in range(n, 0, -1):
+      for num, ngram in enumerate(all_ngrams[i]):
+        ngram_idx[i-1][ngram] = num
+
     discount = (1.0, 0.5, 0.75, 0.75, 0.75, 0.75)
 
-    @memoize
     def prob(full_ngram):
       current_p = 0.0
       p_multiplier = 1.0
+      estimation_base = ngrams
       for i in range(n, 0, -1):
         ngram = full_ngram[-i:]
         prefix = ngram[:-1]
-        estamation_base = ngrams if i == n else unique_prefixes
-        p = max(0, estamation_base[i][ngram] - discount[i]) / estamation_base[i-1][prefix]
-        current_p += p * p_multiplier
-        p_multiplier *= discount[i] / estamation_base[i-1][prefix] * unique_suffixes[i-1][prefix]
+        if estimation_base[i-1][prefix]:
+          #print("i", i)
+          #print("full ngram", full_ngram)
+          #print("ngram", ngram)
+          #print("prefix", prefix)
+          #print("estamition_base", estimation_base[i][ngram])
+          p = max(0, estimation_base[i][ngram] - discount[i]) / estimation_base[i-1][prefix]
+          current_p += p * p_multiplier
+          p_multiplier *= discount[i] / estimation_base[i-1][prefix] * unique_suffixes[i-1][prefix]
+          estimation_base = unique_prefixes
       current_p += p_multiplier / symbol_count # probability of an unseen symbol
       #print(u"Prob of {}: {}".format(''.join(symbol_map[c] for c in ngram), prob_cache[ngram]))
       return current_p
 
-    ngram_db = tuple(list() for i in range(n))
-    ngram_idx = tuple(dict() for i in range(n))
-    ngram_predictions = []
-    n1 = n - 1
-    for index, arr in enumerate(arrays):
-      log("Precomputing ngram probabilities... {}/{}".format(index, len(arrays)))
-      ngram_stack = np.zeros([n], dtype=np.int32)
+    precomputing_started_clock = log("Precomputing successor probabilities")
+    # probabilities for the next symbol based on the last (n-1)-gram
+    ngram_probability_table = np.zeros((len(ngram_idx[-2]), symbol_count), dtype=np.float32)
+    progress = 0
+    for ngram, idx in ngram_idx[-2].items():
+      if progress % 500 == 0:
+        if progress:
+          time_left = (clock() - precomputing_started_clock) / progress * (len(ngram_idx[-2]) - progress)
+        else:
+          time_left = float("inf")
+        log("Precomputing successor probabilities: {:.1f}% ({:.0f} seconds left)".format(100.0 * progress / len(ngram_idx[-2]), time_left))
+      probs = np.array([prob(ngram + (i,)) for i in range(symbol_count)])
+      probs = probs / max(np.sum(probs), 0.0001)
+      #pred_symbol = np.argmax(ngram_probability_table[ngram_idx[n1][prefix]])
+      #print(u"Prefix '{}', prediction {:3d} '{}'".format(''.join(symbol_map[c] for c in prefix), pred_symbol, symbol_map[pred_symbol]))
+      ngram_probability_table[idx, :] = probs
+      progress += 1
 
+    log("Building ngram sequence")
+    ngram_dataset = np.zeros(dataset_np.shape + (n,), dtype=np.int32)
+    offset = 0
+    for array_index, arr in enumerate(arrays):
       for i in range(n):
         ngram_length = i + 1
-        for prefix in ngram_generator(chain((data.GO,) * ngram_length, arr), ngram_length):
-          pass
+        for pos, ngram in enumerate(ngram_generator(chain((data.GO,) * ngram_length, arr), ngram_length)):
+          ngram_dataset[offset + pos, i] = ngram_idx[i][ngram]
+      offset += len(arr) + 2
 
-      for prefix in ngram_generator(chain((data.GO,) * n1, arr), n1):
-        if prefix not in ngram_idx[n1]:
-          probs = np.array([prob(prefix + (i,)) for i in range(symbol_count)])
-          probs = probs / np.sum(probs)
-          ngram_idx[n1][prefix] = len(ngram_db)
-          ngram_db.append(probs)
-        pred_symbol = np.argmax(ngram_db[ngram_idx[n1][prefix]])
-        #print(u"Prefix '{}', prediction {:3d} '{}'".format(''.join(symbol_map[c] for c in prefix), pred_symbol, symbol_map[pred_symbol]))
-        ngram_predictions.append(ngram_idx[prefix])
-      ngram_predictions.append(0)
-    ngram_index = np.array(ngram_predictions)
-    ngram_db = np.array(ngram_db)
-    np.savez(ngram_db_path, concatenated=ngram_index, db=ngram_db)
+    np.savez(ngram_db_path, ngram_dataset=ngram_dataset, ngram_probability_table=ngram_probability_table)
 
 # TODO: retrain ngrams on the test set
 # TODO: use more lengths of n-grams
@@ -109,23 +137,18 @@ def translate2(array):
   return '\n'.join(translate1(row) for row in array)
 
 batch_size = 400
-train_test_divider = int(len(lengths) * 0.8)
-sequence_count = len(lengths)
-test_count = sequence_count - train_test_divider
 check_indices = True
 unrolled_iterations = 300
 network_size = 200
 layer_count = 2
-ngram_count = len(ngram_db)
-
-concatenated_arrays = np.concatenate([np.concatenate(([data.GO], arr, [data.EOS])) for arr in arrays])
+ngram_count = len(ngram_probability_table)
 
 # [sum(len(a) + 2 for a in arrays)] : int32
-dataset = tf.Variable(concatenated_arrays,
-                      trainable=False, dtype=tf.int32, name="flat_dataset")
+dataset = tf.Variable(dataset_np,
+                      trainable=False, dtype=tf.int32, name="dataset")
 
-ngram_index = tf.Variable(ngram_index, trainable=False, dtype=tf.int32, name="ngram_index")
-ngram_db = tf.Variable(ngram_db, trainable=False, dtype=tf.float32, name="ngram_db")
+ngram_dataset = tf.Variable(ngram_dataset, trainable=False, dtype=tf.int32, name="ngram_dataset")
+ngram_probability_table = tf.Variable(ngram_probability_table, trainable=False, dtype=tf.float32, name="ngram_probability_table")
 
 # [num_sequences] : int32
 sequence_lengths = tf.Variable([l+2 for l in lengths], trainable=False, name="sequence_lengths")
@@ -178,9 +201,9 @@ def build_batch(indices, length, progress=None):
   indices_to_gather = tf.add(tf.tile(tf.expand_dims(current_offsets, 1), [1, length + 1]),
                              range_matrix, name='indices_to_gather')
   sequence = tf.gather(dataset, indices_to_gather, check_indices, name="sequence")
-  ngram_index_sequence = tf.gather(ngram_index, tf.slice(indices_to_gather, [0, 0], [-1, length]),
+  ngram_index_sequence = tf.gather(ngram_dataset, tf.slice(indices_to_gather, [0, 0], [-1, length]),
                                    check_indices, name="ngram_index_sequence")
-  ngram_sequence = tf.gather(ngram_db, ngram_index_sequence, check_indices)
+  ngram_sequence = tf.gather(ngram_probability_table, ngram_index_sequence, check_indices)
   # [batch_size] : int32
   # NOTE: batch_max_range is used instead of remaining_batch_lengths to prevent passing of the final EOS as the input
   consumed_sequence_lengths = tf.minimum(max_range, length, name="consumed_sequence_lengths")
@@ -206,7 +229,7 @@ class Train:
     # tuple2(tuple2([batch_size, network_size])) : float32
     network_state = make_state_tuple(initial_zero_state, True)
 
-    global_step = tf.Variable(1, dtype=tf.int32, name='global_step')
+    global_step = tf.Variable(1, dtype=tf.int32, trainable=False, name='global_step')
 
     # [batch_size] : int32
     random_batch_indices = tf.random_uniform([batch_size], minval=0, maxval=train_test_divider, dtype=tf.int32,
@@ -220,11 +243,11 @@ class Train:
     # [batch_size] : int32
     batch_progress = tf.Variable(zero_batch, trainable=False, name="batch_progress")
 
-    input_sequence, expected_sequence, ngram_index_sequence, ngram_sequence, consumed_sequence_lengths, finished_batch_mask =\
+    input_sequence, expected_sequence, ngram_index_sequence, ngram_sequence_list, consumed_sequence_lengths, finished_batch_mask =\
       build_batch(batch_indices, unrolled_iterations, progress=batch_progress)
 
     # [batch_size, unrolled_iterations, symbol_count]
-    input_vector = build_input_vector(input_sequence, ngram_index_sequence, ngram_sequence)
+    input_vector = build_input_vector(input_sequence, ngram_index_sequence, ngram_dataset)
 
     with tf.variable_scope("Cell", reuse=None):
       cell = make_cell(dropout=True)
@@ -232,7 +255,7 @@ class Train:
                                          sequence_length=consumed_sequence_lengths,
                                          initial_state=network_state)
 
-    total_loss, average_loss = get_loss(input_sequence, ngram_sequence, outputs, expected_sequence, consumed_sequence_lengths)
+    total_loss, average_loss = get_loss(input_sequence, ngram_dataset, outputs, expected_sequence, consumed_sequence_lengths)
 
     optimizer = tf.train.AdamOptimizer(args.learning_rate)
     tvars = tf.trainable_variables()
@@ -266,16 +289,16 @@ class Test:
   with tf.name_scope("Test"):
     indices = tf.range(train_test_divider, sequence_count)
     max_length = int(np.max(lengths[train_test_divider:]) + 2)
-    input_sequence, expected_sequence, ngram_index_sequence, ngram_sequence, consumed_sequence_lengths, finished_mask =\
+    input_sequence, expected_sequence, ngram_index_sequence, ngram_sequence_list, consumed_sequence_lengths, finished_mask =\
       build_batch(indices, max_length)
-    input_vector = build_input_vector(input_sequence, ngram_index_sequence, ngram_sequence)
+    input_vector = build_input_vector(input_sequence, ngram_index_sequence, ngram_dataset)
     initial_state = make_state_tuple(tf.zeros([test_count, network_size]), False)
     with tf.variable_scope("Cell", reuse=True):
       cell = make_cell(dropout=False)
       outputs, _ = tf.nn.dynamic_rnn(cell, input_vector,
                                      sequence_length=consumed_sequence_lengths,
                                      initial_state=initial_state)
-    total_loss, average_loss = get_loss(input_sequence, ngram_sequence, outputs, expected_sequence, consumed_sequence_lengths)
+    total_loss, average_loss = get_loss(input_sequence, ngram_dataset, outputs, expected_sequence, consumed_sequence_lengths)
     summaries = tf.merge_summary([tf.scalar_summary('test_average_loss', average_loss)])
 
 init_op = tf.initialize_all_variables()
